085315f6905bc8b0de3ecc61abcc60b98dd04b29
Andrey Archipov <a.arhipov@agimagroup.ru>
2022-07-27 01:42:19 +0400

    fix prev commit

--- components/sections/Description/index.tsx
+++ components/sections/Description/index.tsx
@@ -2,7 +2,6 @@ import {memo} from 'react'
 import {Section} from '@apiModels/sections'
 import {DescriptionType} from '@apiModels/sections/description'
 import useAdditionalAttributes from '@hooks/useAdditionalAttributes'
-import Markdown from 'react-markdown'
 import styles from './Description.module.scss'
 
 
@@ -26,6 +25,6 @@ export const Description = memo<Props>(({section}) => {
             id={id}
             ref={root}
             className={styles.main}
-        ><Markdown>{description}</Markdown></section>
+        ><p>{description}</p></section>
     )
 })
