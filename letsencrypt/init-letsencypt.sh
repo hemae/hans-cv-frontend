a747b6ae2f9cd18dd77d90f965b2633929f3c98a
Andrey Archipov <a.arhipov@agimagroup.ru>
2022-08-05 01:03:36 +0400

    ssl test

--- letsencrypt/init-letsencypt.sh
+++ letsencrypt/init-letsencypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(workcard.fun www.workcard.fun)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="hemae2468@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
